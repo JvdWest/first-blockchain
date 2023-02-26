cb8092aa4cfd71e2ad15843c05c271c46f40065a
JacquesVanDerWesthui <jacquesv@synthesis.co.za>
2023-02-26 10:06:27 +0200

    Emit new task to blockchain

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -10,6 +10,12 @@ contract ToDoList {
 
     mapping(uint => Task) public tasks;
 
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
     constructor() public {
         createTask("Check out university");
     }
@@ -17,5 +23,6 @@ contract ToDoList {
     function createTask(string memory _content ) public {
         taskCount ++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
 }
